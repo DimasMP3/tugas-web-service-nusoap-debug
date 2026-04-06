f714c4dd9cd0b876dc1130804651f1e16e52b4bb
DimasMP3 <dmsmlna123571@gmail.com>
2026-04-06 22:31:59 +0700

    feat: uji struktur code

--- tugas-uji/ujistruktur.php
+++ tugas-uji/ujistruktur.php
@@ -2,4 +2,13 @@
 
 echo "Hello, World!";
 
+$bill1 = 100;
+$bill2 = 200;
+
+$returnplus = $bill1 + $bill2;
+echo "Hasil penjumlahan dari " . $bill1 . " dan " . $bill2 . " adalah " . $returnplus;
+
+$returnminus = $bill1 - $bill2;
+echo "<br> Hasil pengurangan dari " . $bill1 . " dan " . $bill2 . " adalah " . $returnminus;
+
 ?>
